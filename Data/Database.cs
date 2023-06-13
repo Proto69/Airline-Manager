923f62204022bc0d29e418e2900abd9860f1927a
Proto68 <timkaq67@gmail.com>
2023-06-13 17:58:50 +0300

    Created class to get the connection to the database

---
+++ Data/Database.cs
@@ -0,0 +1,14 @@
+﻿namespace Airline_Manager.Data
+{
+    public static class Database
+    {
+        // The connection string (put your info here)
+        private static string connectionString = "Server=localhost;Database=airline_manager;Uid=root;Pwd=12345";
+
+        // Method that returns a connection created with the string
+        public static MySqlConnection GetConnection()
+        {
+            return new MySqlConnection(connectionString);
+        }
+    }
+}
