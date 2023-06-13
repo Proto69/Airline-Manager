1b916a4e242e8cb0eb72b00104c94c4d39ebc3ee
Proto68 <timkaq67@gmail.com>
2023-06-13 17:58:29 +0300

    Added class to control the color of the console font

---
+++ ConsoleColor.cs
@@ -0,0 +1,26 @@
+﻿namespace Airline_Manager
+{
+    // Class for controlling the console font color
+    public static class ConsoleColor
+    {
+        public static void Green()
+        {
+            Console.ForegroundColor = System.ConsoleColor.Green;
+        }
+
+        public static void Red()
+        {
+            Console.ForegroundColor = System.ConsoleColor.Red;
+        }
+
+        public static void Yellow()
+        {
+            Console.ForegroundColor = System.ConsoleColor.Yellow;
+        }
+
+        public static void Reset()
+        {
+            Console.ResetColor();
+        }
+    }
+}
