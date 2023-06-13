58c6018750e98d3295dc9a1c96feecdfe7a747ce
Proto68 <timkaq67@gmail.com>
2023-06-13 16:14:06 +0300

    Add project files.

---
+++ DataCollector/AirportData.cs
@@ -0,0 +1,30 @@
+﻿namespace Airline_Manager.DataCollector
+{
+    public static class AirportData
+    {
+        private static string filePath = "C:\\Users\\mnogo\\source\\repos\\Airline Manager\\TextFiles\\GlobalAirportDatabase.txt";
+
+        public static string GetAirport(string keyword)
+        {
+            string[] lines = File.ReadAllLines(filePath);
+
+            string result = "";
+
+            foreach (string line in lines)
+            {
+                if (line.Contains(keyword.ToUpper()))
+                {
+                    string[] infoArray = line.Split(':');
+                    string code = infoArray[1];
+                    string city = infoArray[2];
+                    string country = infoArray[4];
+                    string n = infoArray[14];
+                    string e = infoArray[15];
+                    result = code + ";" + city + ";" + country + ";" + n + ";" + e;
+                    break;
+                }
+            }
+            return result;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Airline_Manager
+{
+    internal class Program
+    {
+        static void Main()
+        {
+
+        }
+    }
+}
