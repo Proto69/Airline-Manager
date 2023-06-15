3dfb6d33cf505dcdca52ede5d6f3e21a7d168116
Proto68 <timkaq67@gmail.com>
2023-06-15 20:55:19 +0300

    Changed the route data

--- Data/RouteData.cs
+++ Data/RouteData.cs
@@ -6,12 +6,13 @@
         {
             MySqlConnection conn = Database.GetConnection();
 
-            string query_add_route = "INSERT INTO routes (`from`, `to`, km) VALUES (@from, @to, @km, @daily_demand);";
+            string query_add_route = "INSERT INTO routes (name, `from`, `to`, km) VALUES (@name, @from, @to, @km);";
             MySqlCommand cmd_add_route = new MySqlCommand(query_add_route, conn);
 
             cmd_add_route.Parameters.AddWithValue("@from", route.From);
             cmd_add_route.Parameters.AddWithValue("@to", route.To);
             cmd_add_route.Parameters.AddWithValue("@km", route.Km);
+            cmd_add_route.Parameters.AddWithValue("@name", route.Name);
 
             conn.Open();
             cmd_add_route.ExecuteNonQuery();
