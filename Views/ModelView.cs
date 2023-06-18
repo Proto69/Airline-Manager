d1796eb9e2d58bbe18ac5d214b31cd41554a7cbc
Proto68 <timkaq67@gmail.com>
2023-06-18 08:41:14 +0300

    Done with displaying all aircrafts

--- Views/ModelView.cs
+++ Views/ModelView.cs
@@ -1,4 +1,6 @@
-﻿namespace Airline_Manager.Views
+﻿using Airline_Manager.Models;
+
+namespace Airline_Manager.Views
 {
     public static class ModelView
     {
@@ -48,13 +50,90 @@
 
         public static void DisplayAllAircrafts(List<Aircraft> aircrafts)
         {
-            int max = 0;
+            Console.WriteLine();
+            int max = 10;
             foreach (Aircraft aircraft in aircrafts)
             {
-                int n = aircraft.Model.Length + aircraft.Route.Name.Length;
+                int n = aircraft.Model.Length;
+                if (n > max)
+                    max = n;
+                n = aircraft.Route.Name.Length;
                 if (n > max)
                     max = n;
             }
+            max += 2;
+            PrintWord(" Model", max);
+            PrintCol("|");
+            PrintWord(" Route name", max);
+            PrintCol("|");
+            PrintWord(" Status", max);
+            PrintCol("|");
+            PrintWord(" Landing in", max);
+
+            NewRow(max);
+
+            foreach (Aircraft aircraft in aircrafts)
+            {
+                PrintAircraft(aircraft, max);
+            }
+        }
+
+        private static void PrintCol(string n)
+        {
+            ConsoleColor.Blue();
+            Console.Write(n);
+            ConsoleColor.Reset();
+        }
+
+        private static void PrintWord(string word, int max)
+        {
+            int a = word.Length;
+            int b = max - a;
+            string result = word + new string(' ', b);
+            Console.Write(result);
+        }
+
+        private static void NewRow(int max)
+        {
+            Console.WriteLine();
+            ConsoleColor.Blue();
+            Console.Write(new string('-', max));
+            PrintCol("|");
+            ConsoleColor.Blue();
+            Console.Write(new string('-', max));
+            PrintCol("|");
+            ConsoleColor.Blue();
+            Console.Write(new string('-', max));
+            PrintCol("|");
+            ConsoleColor.Blue();
+            Console.WriteLine(new string('-', max));
+            ConsoleColor.Reset();
+        }
+
+        private static void PrintAircraft(Aircraft aircraft, int max)
+        {
+            PrintWord(" " + aircraft.Model, max);
+            PrintCol("|");
+            PrintWord(" " + aircraft.Route.Name, max);
+            PrintCol("|");
+            if (aircraft.Airborne)
+            {
+                PrintWord(" Flying", max);
+                PrintCol("|");
+                ConsoleColor.Green();
+                PrintWord(" " + TimeChecker.TimeLeft(aircraft), max);
+                ConsoleColor.Reset();
+            }
+            else
+            {
+                PrintWord(" Landed", max);
+                PrintCol("|");
+                ConsoleColor.Red();
+                PrintWord(" N/A", max);
+                ConsoleColor.Reset();
+            }
+
+            NewRow(max);
         }
     }
 }
