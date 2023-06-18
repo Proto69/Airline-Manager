7e2b2d42423aa2b35626fb06bc9125d2e4d4a1cd
Proto68 <timkaq67@gmail.com>
2023-06-18 08:42:12 +0300

    Started adding various commands

--- Controllers/Engine.cs
+++ Controllers/Engine.cs
@@ -8,11 +8,26 @@
             if (GameSaver.CheckGame())
                 ProfileSetup.NewSetup();
 
+            TimeChecker.CheckTime();
+            ModelView.DisplayAllAircrafts(GlobalVariables.OwnedAircrafts);
+
             while (true)
             {
                 try
                 {
+                    TimeChecker.CheckTime();
+                    ModelView.SendMessage("Write a command, for help type HELP:");
+                    string[] input = Console.ReadLine().Split(" ").ToArray();
+                    switch (input[0])
+                    {
+                        case "":
+                            ModelView.DisplayAllAircrafts(GlobalVariables.OwnedAircrafts);
+                            break;
 
+                        case "HELP":
+                            // List all commands
+                            break;
+                    }
                 }
                 catch (Exception e)
                 {
